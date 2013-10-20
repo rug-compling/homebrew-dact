2b7a4da341ce9fee44b06343dd3074fe7d75c774
Daniël de Kok <me@danieldk.eu>
2013-10-20 18:01:01 +0200

    Update alpinocorpus to version 2.0.3.

--- alpinocorpus.rb
+++ alpinocorpus.rb
@@ -1,22 +1,21 @@
 require 'formula'
 
 class Alpinocorpus < Formula
+	url 'https://github.com/rug-compling/alpinocorpus/archive/2.0.3.tar.gz'
+	homepage 'http://github.com/rug-compling/alpinocorpus'
+	head 'https://github.com/rug-compling/alpinocorpus.git'
+	sha1 '095df854d9c706919d7d0e7c4ac5bab0ffff5db3'
+
 	depends_on 'cmake' => :build
 	depends_on 'boost'
 	depends_on 'dbxml'
 
-	url 'https://github.com/rug-compling/alpinocorpus/tarball/2.0.2'
-	homepage 'http://github.com/rug-compling/alpinocorpus'
-	head 'https://github.com/rug-compling/alpinocorpus.git'
-	sha1 '143b4d614f64b7fff312d48257a99b556508df8e'
-
 	def install
-		args = std_cmake_args
-		args << '-DUSE_REMOTE_CORPUS:BOOL=OFF'
+		cmake_args = std_cmake_args
+		cmake_args << '-DUSE_REMOTE_CORPUS:BOOL=OFF'
 
-		mkdir 'build'
-		cd 'build' do
-			system 'cmake', '..', *args
+		mkdir 'build' do
+			system 'cmake', '..', *cmake_args
 			system 'make install'
 		end
 	end
