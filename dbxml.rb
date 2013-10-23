d6bf74729c9b237e39c95254c49d69880b02b603
Daniël de Kok <me@danieldk.eu>
2013-10-23 11:44:16 +0200

    Update gist URLs.

--- dbxml.rb
+++ dbxml.rb
@@ -6,7 +6,7 @@ class Dbxml < Formula
   md5 '2732618d5c4f642fba3d2a564a025986'
 
   def patches
-  	{ :p1 => ["https://raw.github.com/gist/3761290/365a3c2077191530216faaf97ecbadaacb9efc2f/fix-gcc-4.7-build.patch", "https://raw.github.com/gist/3986724/f27306c03f42a85a43fc667d5c57a69ec70a2920/UnionQP-segfault.diff", "https://gist.github.com/danieldk/5700533/raw/883b623a91b2ace2ddc69c1b8a13792acc283640/gistfile1.txt"]}
+  	{ :p1 => ["https://gist.github.com/danieldk/3761290/raw/365a3c2077191530216faaf97ecbadaacb9efc2f/fix-gcc-4.7-build.patch", "https://gist.github.com/danieldk/3986724/raw/f27306c03f42a85a43fc667d5c57a69ec70a2920/UnionQP-segfault.diff", "https://gist.github.com/danieldk/5700533/raw/883b623a91b2ace2ddc69c1b8a13792acc283640/gistfile1.txt"]}
   end
 
   def install
