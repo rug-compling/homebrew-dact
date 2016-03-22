44662f7131ced6b1a3a3e3e5f072b10f3d482710
Daniël de Kok <me@danieldk.eu>
2016-03-22 10:28:10 +0100

    SHA-256 checksum

--- dbxml.rb
+++ dbxml.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Dbxml < Formula
   url 'http://download.oracle.com/berkeley-db/dbxml-2.5.16.tar.gz'
   homepage 'http://www.oracle.com/us/products/database/berkeley-db/index-066571.html'
-  md5 '2732618d5c4f642fba3d2a564a025986'
+  sha256 'f2423be3805811a9cca71d6877c0b1bdc1941cd76c3f26d6eb011a5724a3edd2'
 
   def patches
   	{ :p1 => ["https://gist.github.com/danieldk/3761290/raw/365a3c2077191530216faaf97ecbadaacb9efc2f/fix-gcc-4.7-build.patch", "https://gist.github.com/danieldk/3986724/raw/f27306c03f42a85a43fc667d5c57a69ec70a2920/UnionQP-segfault.diff", "https://gist.github.com/danieldk/5700533/raw/883b623a91b2ace2ddc69c1b8a13792acc283640/gistfile1.txt"]}
