2565edddd5272b1ea9553a2c9d1bb629b96a4034
Daniël de Kok <me@danieldk.eu>
2017-01-21 10:55:29 +0100

    Update alpinocorpus to 2.6.0.

--- alpinocorpus.rb
+++ alpinocorpus.rb
@@ -1,10 +1,10 @@
 require 'formula'
 
 class Alpinocorpus < Formula
-	url 'https://github.com/rug-compling/alpinocorpus/archive/2.4.1.tar.gz'
+	url 'https://github.com/rug-compling/alpinocorpus/archive/2.6.0.tar.gz'
 	homepage 'http://github.com/rug-compling/alpinocorpus'
 	head 'https://github.com/rug-compling/alpinocorpus.git'
-	sha256 'a6ae9611ceed304ee886e348ebaae7189d210a602d7b26e24bedd5c503469846'
+	sha256 'be9b30c0b3cb13b6a710bc4cc96e526d1a61864c2b48914a2c7e182d43135d9f'
 
 	depends_on 'cmake' => :build
 	depends_on 'boost'
--- dact.rb
+++ dact.rb
@@ -1,25 +1,26 @@
 require 'formula'
 
 class Dact < Formula
-	url 'https://github.com/rug-compling/dact/archive/2.4.4.tar.gz'
-	homepage 'http://github.com/rug-compling/dact'
-	head 'https://github.com/rug-compling/dact.git'
-	sha256 '7a1ecaa0622d759c317d7236ec7ef9e13224b39def7cd58d1a29a3ae06ab328b'
+  url 'https://github.com/rug-compling/dact/archive/2.4.4.tar.gz'
+  homepage 'http://github.com/rug-compling/dact'
+  head 'https://github.com/rug-compling/dact.git'
+  sha256 '7a1ecaa0622d759c317d7236ec7ef9e13224b39def7cd58d1a29a3ae06ab328b'
+  revision 1
 
-	depends_on 'cmake' => :build
-	depends_on 'rug-compling/dact/alpinocorpus'
-	depends_on 'qt5'
+  depends_on 'cmake' => :build
+  depends_on 'rug-compling/dact/alpinocorpus'
+  depends_on 'qt5'
 
-	def install
-		args = std_cmake_args
-		args << '-DUSE_REMOTE_CORPUS:BOOL=OFF'
-		args << "-DUSE_WEBSERVICE:BOOL=OFF"
-		args << "-DENABLE_BUNDLE:BOOL=OFF"
+  def install
+    args = std_cmake_args
+    args << '-DUSE_REMOTE_CORPUS:BOOL=OFF'
+    args << "-DUSE_WEBSERVICE:BOOL=OFF"
+    args << "-DENABLE_BUNDLE:BOOL=OFF"
 
-		mkdir 'build'
-		cd 'build' do
-			system 'cmake', '..', *args
-			system 'make install'
-		end
-	end
+    mkdir 'build'
+    cd 'build' do
+      system 'cmake', '..', *args
+      system 'make install'
+    end
+  end
 end
