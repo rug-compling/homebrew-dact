7f8e5f3741334db10b9e80bd8c6745e4cbab4e0c
Daniël de Kok <me@danieldk.eu>
2018-01-04 14:17:01 +0100

    alpinocorpus 2.8.0

--- alpinocorpus.rb
+++ alpinocorpus.rb
@@ -1,18 +1,17 @@
 require 'formula'
 
 class Alpinocorpus < Formula
-	url 'https://github.com/rug-compling/alpinocorpus/archive/2.6.0.tar.gz'
+	url 'https://github.com/rug-compling/alpinocorpus/archive/2.8.0.tar.gz'
 	homepage 'http://github.com/rug-compling/alpinocorpus'
 	head 'https://github.com/rug-compling/alpinocorpus.git'
-	sha256 'be9b30c0b3cb13b6a710bc4cc96e526d1a61864c2b48914a2c7e182d43135d9f'
+	sha256 '5d3fb5cf9d0e6b18195c789e81686cbb8d759cfd753de8ae189c1e593412d566'
 
 	depends_on 'cmake' => :build
 	depends_on 'boost'
-	depends_on 'legacy-dbxml'
+	depends_on 'patched-dbxml'
 
 	def install
 		cmake_args = std_cmake_args
-		cmake_args << '-DUSE_REMOTE_CORPUS:BOOL=OFF'
 
 		mkdir 'build' do
 			system 'cmake', '..', *cmake_args
