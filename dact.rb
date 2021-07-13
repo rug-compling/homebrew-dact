be1e644497a1744218cc0c350ef97fce06c07e53
Daniël de Kok <me@danieldk.eu>
2021-07-13 20:22:31 +0200

    dact: update to 3.0.0 beta

--- dact.rb
+++ dact.rb
@@ -1,26 +1,23 @@
 require 'formula'
 
 class Dact < Formula
-  url 'https://github.com/rug-compling/dact/archive/2.6.2.tar.gz'
+  url 'https://github.com/rug-compling/dact/archive/e38f503.tar.gz'
   homepage 'http://github.com/rug-compling/dact'
   head 'https://github.com/rug-compling/dact.git'
-  sha256 '0b20d8e2945b0715c9f9af0af99ad910de6730bf0097e1681cdd2cda0d7b8068'
+  sha256 '3a0cc8230312d191a6c2c7562e8d683f9d27e28e07f948c0309a8f386b38d308'
   revision 1
 
-  depends_on 'cmake' => :build
+  depends_on "meson" => :build
+  depends_on "ninja" => :build
   depends_on 'rug-compling/dact/alpinocorpus'
   depends_on 'qt5'
 
   def install
-    args = std_cmake_args
-    args << '-DUSE_REMOTE_CORPUS:BOOL=OFF'
-    args << "-DUSE_WEBSERVICE:BOOL=OFF"
-    args << "-DENABLE_BUNDLE:BOOL=OFF"
-
     mkdir 'build'
     cd 'build' do
-      system 'cmake', '..', *args
-      system 'make install'
+      system "meson", *std_meson_args, ".."
+      system "ninja", "-v"
+      system "ninja", "install", "-v"
     end
   end
 end
